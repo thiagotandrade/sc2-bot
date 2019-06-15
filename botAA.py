367e593a6ea873d1550f799ec29894dddbbcef5d
Thiago Andrade <ttca@cin.ufpe.br>
2019-06-15 15:23:56 -0300

    better managing bases, workers and buildings

--- botAA.py
+++ botAA.py
@@ -1,14 +1,19 @@
 '''
     bot_ai.py:
         Linha 518. O parametro max_distance por padrão é 20 (está 8)
+    
+    Ideias:
     -> Implementar as estratégias de early e late
-    -> Melhorar a localização na construção das estruturas (tá travando os workers)
     -> Cancelar a construção de estruturas que estejam sendo atacadas.
-
+    -> Definir quais unidades inimigas devem ser atacadas primeiro (priorizar a unidade mais "valiosa" que esteja mais perto)
     Sobre Upgrades:
         It usually comes down to preference but most people will tell you to prioritize weapons upgrades against Zerg and fellow Protoss, and to prioritize armor upgrades against Terran.
         Keep in mind this is when working off one Forge, as soon as you get two Forges that you can continuously upgrade out of, this is irrelevant.
         Also, save shields for last ALWAYS
+
+    função main_base_ramp
+
+    Olhar get_available_abilities no bot_ai pra usar as habilidades das unidades quando for batalhar
 '''
 import random
 
@@ -26,6 +31,9 @@ class BotAA(sc2.BotAI):
         self.actions_list = []
         self.iteration = 0
         self.strategy = "e"
+        self.max_workers = 75
+        self.gateways_per_nexus = 2
+        self.enemy_units = []        
         
 
     async def on_step(self, iteration):
@@ -36,17 +44,20 @@ class BotAA(sc2.BotAI):
         elif self.strategy == "l":
             await self.late_game_strategy()
 
+        if self.strategy == "e" and (self.time > 180 or len(self.units(NEXUS)) >= 2 or len(self.units(STALKER)) >= 10):
+            self.strategy = "l"
+
         if self.iteration % 10 == 0:
             await self.distribute_workers()
+            #await self.chat_send(str(self.time) + ", " + self.time_formatted)
 
-        await self.build_workers()
-        await self.build_pylons()
-        await self.build_assimilators()
         await self.expand()
+        await self.build_pylons()
         await self.manage_bases()
         await self.offensive_force_buildings()
         await self.build_offensive_force()
         await self.attack()
+        await self.idle_workers()
       
         #Execute all orders
         await self.execute_actions_list()
@@ -57,54 +68,80 @@ class BotAA(sc2.BotAI):
 
     # TODO: implement early game strategy
     async def early_game_strategy(self):
+        prefered_base_count = 2
+
+        #if self.units(NEXUS).amount < prefered_base_count:
+            
         '''
-            Treinar probes (Já é feito por outra função) 
+            construir algumas defesas
             construir pylon > gateway > cybernetics > pylon
             assim que gateway estiver pronto, construir 1 zealot
-            assim que cybernetics core estiver pronto, construir 1 adept
+            assim que cybernetics core estiver pronto
+                * começar a construir stalkers
+                * começar a pesquisar warpgate
+            construir forge
         '''
+        
         return
+
 
     # TODO: implement late game strategy
     async def late_game_strategy(self):
+        
+        # Make sure to expand in late game (every 2.5 minutes)
+        expand_every = 2.5 * 60 # Seconds
+        prefered_base_count = 1 + int(self.time / expand_every)
+        prefered_base_count = max(prefered_base_count, 2) # Take natural ASAP (i.e. minimum 2 bases)
+        current_base_count = self.units(NEXUS).ready.filter(lambda unit: unit.ideal_harvesters >= 10).amount # Only count bases as active if they have at least 10 ideal harvesters (will decrease as it's mined out)
+
+        '''
+            Construir robotics bay, twilight consul para construir colossus e dark templar
+            Pesquisar thermal lance e psionic storm
+        '''
         return
 
-    # Execute all orders in self.order_queue and reset it
+
+    # Execute all orders in self.actions_list and reset it
     async def execute_actions_list(self):
         await self.do_actions(self.actions_list)
-        self.actions_list = [] # Reset order queue
+        self.actions_list = [] # Reset actions list
+              
+    async def build_workers(self, nexus):
+        if nexus.is_idle and self.can_afford(PROBE) and nexus.assigned_harvesters <= (nexus.ideal_harvesters + 3) and self.workers.amount < self.max_workers and self.supply_used < 180:
+            await self.do(nexus.train(PROBE))
 
-
-    async def build_workers(self):
-        for nexus in self.units(NEXUS).ready.idle:
-            if self.can_afford(PROBE):
-                await self.do(nexus.train(PROBE))
-    
+    # Force idle workers near nexus to mine
+    async def idle_workers(self):
+        if self.workers.idle.exists:
+            worker = self.workers.idle.first
+            await self.do(worker.gather(self.state.mineral_field.closest_to(self.units(NEXUS).random)))
+            
 
     async def build_pylons(self):
+        nexus = self.units(NEXUS).random
+        if not nexus:
+            return
+
         # supply_left: Available Population to Produce 
-        if self.supply_left <= 6 and not self.already_pending(PYLON) and self.supply_cap < 200:
-            nexuses = self.units(NEXUS).ready
-            if nexuses.exists:
+        if self.supply_left <= 6 and not self.already_pending(PYLON) and self.supply_cap < 190:
                 if self.can_afford(PYLON):
-                    await self.build(PYLON, near=nexuses.first)
+                    await self.build(PYLON, near=nexus.position.towards(self.game_info.map_center, 10))
 
 
-    async def build_assimilators(self):
-        for nexus in self.units(NEXUS).ready:
-            vespenes = self.state.vespene_geyser.closer_than(15.0, nexus)
-            for vespene in vespenes:
-                if not self.can_afford(ASSIMILATOR):
-                    break
-                worker = self.select_build_worker(vespene.position)
-                if worker is None:
-                    break
-                if not self.units(ASSIMILATOR).closer_than(1.0, vespene).exists:
-                    await self.do(worker.build(ASSIMILATOR, vespene))
+    async def build_assimilators(self, nexus):
+        vespenes = self.state.vespene_geyser.closer_than(15.0, nexus)
+        for vespene in vespenes:
+            if not self.can_afford(ASSIMILATOR):
+                break
+            worker = self.select_build_worker(vespene.position)
+            if worker is None:
+                break
+            if not self.units(ASSIMILATOR).closer_than(1.0, vespene).exists:
+                await self.do(worker.build(ASSIMILATOR, vespene))
 
 
     async def expand(self):
-        if self.units(NEXUS).amount < 3  and self.can_afford(NEXUS):
+        if self.can_afford(NEXUS) and not self.already_pending(NEXUS):
             await self.expand_now()
 
 
@@ -113,10 +150,10 @@ class BotAA(sc2.BotAI):
             pylon = self.units(PYLON).ready.random
             if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
                 if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
-                    await self.build(CYBERNETICSCORE, near=pylon)
+                    await self.build(CYBERNETICSCORE, near=pylon.position.towards(self.game_info.map_center, 2))
             elif len(self.units(GATEWAY)) < 3:
                 if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
-                    await self.build(GATEWAY, near=pylon)
+                    await self.build(GATEWAY, near=pylon.position.towards(self.game_info.map_center, 2))
 
 
     async def build_offensive_force(self):
@@ -148,7 +185,9 @@ class BotAA(sc2.BotAI):
         # Do some logic for each nexus
         for nexus in self.units(NEXUS).ready:
 
+            await self.build_workers(nexus)
             await self.handle_chronoboost(nexus)
+            await self.build_assimilators(nexus)
 
 
     # Check if a unit has an ability available
@@ -201,6 +240,7 @@ class BotAA(sc2.BotAI):
                     if not gateway.is_idle and not gateway.has_buff(CHRONOBOOSTENERGYCOST):
                         await self.do(nexus(EFFECT_CHRONOBOOSTENERGYCOST, gateway))
                         return # Don't CB anything else this step
+
 
 def main():
     sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
